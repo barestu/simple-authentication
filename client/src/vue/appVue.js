ac6949036c81447eef6120b8c8a3e0d7b1bf95f0
barestu <fheartnet@gmail.com>
2018-04-12 14:57:51 +0700

    Submit

--- client/src/vue/appVue.js
+++ client/src/vue/appVue.js
@@ -6,21 +6,33 @@ new Vue({
     confirmPassword: '',
     email: '',
     password: '',
+    error: '',
+    errorLogin : '',
+    success: '',
+    successLogin: '',
     token: ''
   },
   methods: {
-    register: function(email, password) {
-      console.log(email, password)
-      axios.post('http://localhost:3000/user/register', {
-        email: email,
-        password: password
-      })
-      .then(response => {
-        console.log('Register success', response.data)
-      })
-      .catch(err => {
-        console.log('Register failed', err.message)
-      })
+    register: function(email, password, confirm) {
+      console.log(email, password, confirm)
+      if (email == '' || password == '') {
+        this.error = 'Email & password required!'
+      } else if (password === confirm && password !== '') {
+        axios.post('http://localhost:3000/user/register', {
+          email: email,
+          password: password
+        })
+        .then(response => {
+          this.success = 'Register success!'
+          location.reload()
+        })
+        .catch(err => {
+          console.log('Register failed', err)
+        })
+      } else {
+        console.log('Your confirm password is not same!')
+        this.error = 'Your confirm password is not same!'
+      }
     },
 
     login: function(email, password) {
@@ -30,11 +42,21 @@ new Vue({
         password: password
       })
       .then(response => {
-        console.log('Login success', response.data.token)
+        this.successLogin = 'Login success' 
+        this.token = response.data.token
       })
       .catch(err => {
-        console.log('Login failed')
+        this.errorLogin = 'Wrong email/password!'
       })
+    }
+  },
+
+  watch: {
+    checkEmail: function() {
+      let re = /\S+@\S+\.\S+/;
+      if(!re.test(email)) {
+        this.error = 'Wrong email format'
+      }
     }
   }
 })
